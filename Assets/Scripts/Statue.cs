4507a25b12367072b3ea4ead84774cd6ce20e956
Ofril <Olahad24@gmail.com>
2023-05-21 00:05:13 +0300

    adding key drop from statue

---
+++ Assets/Scripts/Statue.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Statue : MonoBehaviour
+{
+    public PlayerInventory playerInventory;
+    public GameObject key;
+    private const COINS = 10;
+
+    private void OnCollisionEnter(Collision collision) 
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            if (GetCoinsAmount() < COINS)
+                GiveHint();
+            else
+                GiveKey();
+        }
+    }
+
+    public int GetCoinsAmount()
+    {
+        return playerInventory.NumberOfCoins;
+    }
+
+    private void GiveHint()
+    {
+        Debug.Log("You want the key out? pay me " + COINS + " coins");
+    }
+
+    private void GiveKey()
+    {
+        key.SetActive(true);
+        gameObject.SetActive(false);
+    }
+}
