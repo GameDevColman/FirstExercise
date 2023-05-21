f1424b3725145557bcbbcdfe17ec86c2a496e7f4
dandiv <dandiv32@gmail.com>
2023-05-21 11:33:32 +0300

    added text

---
+++ Assets/Scripts/DialogUI.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DialogUI : MonoBehaviour
+{
+     private TextMeshProUGUI dialogText;
+
+     void Start()
+      {
+          dialogText = GetComponent<TextMeshProUGUI>();
+          Invoke("HideText", 5);
+      }
+
+     public void UpdateDialogText(PlayerInventory playerInventory)
+      {
+         dialogText.text = playerInventory.dialogText;
+         Invoke("HideText", 5);
+      }
+
+      void HideText()
+      {
+        dialogText.text = "";
+      }
+}
