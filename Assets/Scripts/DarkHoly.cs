e441e9d3d73b96d6d9d70d0ed8924ea0d50dd5b0
Ofril <Olahad24@gmail.com>
2023-05-21 12:31:00 +0300

    adding dark holy

--- Assets/Scripts/DarkHoly.cs
+++ Assets/Scripts/DarkHoly.cs
@@ -2,19 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BrightHoly : MonoBehaviour
+public class DarkHoly : MonoBehaviour
 {
-    public GameObject key;
+    public GameObject hiddenCoins;
 
     private void OnTriggerEnter(Collider collider) 
     {
-        ParticleSystem ps = GameObject.Find("BrightHolyExplosion").GetComponent<ParticleSystem>();
+        ParticleSystem ps = GameObject.Find("DarkHolyExplosion").GetComponent<ParticleSystem>();
         
         if (collider.gameObject.CompareTag("Player"))
         {
             ps.Play();
             Destroy(gameObject);
-            key.SetActive(true);
+            hiddenCoins.SetActive(true);
         }
     }
 }
